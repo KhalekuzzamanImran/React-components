0850a12f15da3168eda5e55f2cf4e4c974be3c52
Imran Khan <khalekuzzamanimran@gmail.com>
2023-12-04 22:13:42 +0600

    even-handler-1

--- src/components/ListGroups.tsx
+++ src/components/ListGroups.tsx
@@ -2,15 +2,21 @@ import { Fragment } from "react";
 
 function ListGroups() {
   let items = ["New york", "San Francisco", "Tokyo", "London", "Paris"];
-  items = [];
+
   let msg = items.length === 0 && <p>No item found!</p>;
   return (
     <Fragment>
       <h2>List Groups</h2>
       {msg}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}>{item}</li>
+        {items.map((item, index) => (
+          <li
+            className="list-group-item"
+            key={item}
+            onClick={() => console.log(item, index)}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </Fragment>
