fff18d99d1f3c3badea208e82e049e37de8c3e75
Imran Khan <khalekuzzamanimran@gmail.com>
2023-12-04 21:01:49 +0600

    list-group-with-normalway

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import ListGroups from "./components/ListGroups";
+import Message from "./components/Message";
+
+function App() {
+  return (
+    <div>
+      <>
+        <Message />
+        <ListGroups />
+      </>
+    </div>
+  );
+}
+
+export default App;
