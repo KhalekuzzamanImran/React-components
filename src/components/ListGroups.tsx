b18800d249b53204e0c012b574794d755f61ffa4
Imran Khan <khalekuzzamanimran@gmail.com>
2023-12-04 22:40:15 +0600

    active-item-using-useState

--- src/components/ListGroups.tsx
+++ src/components/ListGroups.tsx
@@ -1,19 +1,27 @@
 import { Fragment } from "react";
-import { MouseEvent } from "react";
+import { useState } from "react";
 
 function ListGroups() {
   let items = ["New york", "San Francisco", "Tokyo", "London", "Paris"];
 
   let msg = items.length === 0 && <p>No item found!</p>;
-  // Event handler
-  const handleClick = (event: MouseEvent) => console.log(event);
+  // Hook (useState)
+  const [selectedIndex, setSelectedIndex] = useState(-1);
   return (
     <Fragment>
       <h2>List Groups</h2>
       {msg}
       <ul className="list-group">
-        {items.map((item) => (
-          <li className="list-group-item" key={item} onClick={handleClick}>
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => setSelectedIndex(index)}
+          >
             {item}
           </li>
         ))}
