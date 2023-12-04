3cd0cb7b5aa6068e28e5ba8f8dc00e67f8dae2f1
Imran Khan <khalekuzzamanimran@gmail.com>
2023-12-04 22:02:56 +0600

    no-item-found-logic-1

--- src/components/ListGroups.tsx
+++ src/components/ListGroups.tsx
@@ -1,10 +1,13 @@
 import { Fragment } from "react";
 
 function ListGroups() {
-  const items = ["New york", "San Francisco", "Tokyo", "London", "Paris"];
+  let items = ["New york", "San Francisco", "Tokyo", "London", "Paris"];
+  items = [];
+  let msg = items.length === 0 ? <p>No item found!</p> : null;
   return (
     <Fragment>
       <h2>List Groups</h2>
+      {msg}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
